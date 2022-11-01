5d9fbf57606b4cfbde2445315801be0c52fd8cfb
Gluuue <93495480+Gluuue@users.noreply.github.com>
2022-11-01 15:22:38 -0400

    Finished Basic Jumping

--- Donkey Kong/Assets/Scripts/Player.cs
+++ Donkey Kong/Assets/Scripts/Player.cs
@@ -1,6 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
-using System.Diagnostics;
 using UnityEngine;
 
 public class Player : MonoBehaviour
@@ -8,6 +5,7 @@ public class Player : MonoBehaviour
     private new Rigidbody2D rigidbody;
     private Vector2 direction;
     public float moveSpeed = 1f;
+    public float jumpStrength = 1f;
 
     private void Awake()
     {
@@ -16,8 +14,26 @@ public class Player : MonoBehaviour
 
     private void Update()
     {
+        if (Input.GetButtonDown("Jump"))
+        {
+            direction = Vector2.up * jumpStrength;
+        } else
+        {
+            direction += Physics2D.gravity * Time.deltaTime;
+        }
+
+
         direction.x = Input.GetAxis("Horizontal") * moveSpeed;
-        direction.y = Input.GetAxis("Vertical") * moveSpeed;
+        direction.y = Mathf.Max(direction.y, -1f);
+
+        if (direction.x > 0f)
+        {
+            transform.eulerAngles = Vector3.zero;
+        }
+        else if (direction.x < 0f)
+        {
+            transform.eulerAngles = new Vector3(0f, 180f, 0f);
+        }
     }
 
     private void FixedUpdate()
