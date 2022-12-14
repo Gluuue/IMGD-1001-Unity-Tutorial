5b2a714363f5374f3d1347ffb4d6ab4287df9941
PaulGodinez452 <89924917+PaulGodinez452@users.noreply.github.com>
2022-12-14 12:49:08 -0500

    Scuffed Scene Switch

--- Donkey Kong/Assets/Scripts/Player.cs
+++ Donkey Kong/Assets/Scripts/Player.cs
@@ -56,6 +56,7 @@ public class Player : MonoBehaviour
 
     private void Awake()
     {
+        DontDestroyOnLoad(this);    
         rigidbody = GetComponent<Rigidbody2D>();
         collider = GetComponent<Collider2D>();
         //Should no longer need this array
---
+++ Donkey Kong/Assets/Scripts/SceneSwitcher.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class SceneSwitcher : MonoBehaviour
+{
+   public int SceneNumber;
+   public int xValue;
+   public int yValue;
+   private void OnCollisionEnter2D(Collision2D collision) {
+          SceneManager.LoadScene(SceneNumber);
+          GameObject player = collision.gameObject;
+          player.transform.position = new Vector2(xValue,yValue);
+    }
+}
